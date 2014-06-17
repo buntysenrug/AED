a5806b862872490ab3b023e4d9c421b58c3c3852
Bunty <bunty.newcastle@gmail.com>
2014-06-17 17:43:16 +0100

    Fresh Repo Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word=Microsoft.Office.Interop.Word;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Word.Application w = new Word.Application();
+            Word.Document doc = null;
+            doc = w.Documents.Open(@"C:\Users\b1036970\Desktop\ABowen_FINALDISSERTATION1.docx");
+            w.Visible = false;
+            foreach (Word.Paragraph p in doc.Paragraphs)
+            {
+                float f = p.SpaceAfter;
+                Console.WriteLine(p.PageBreakBefore);
+            
+                
+            }
+
+            
+            w.Quit();
+           */
+            String filename="C:\\Users\\b1036970\\Desktop\\ABowen_FINALDISSERTATION1.docx";
+            Styles s = new Styles(filename);
+
+            Dictionary<Word.Style, float> dict = s.getSpacingBefore();
+            foreach (var entry in dict)
+                Console.WriteLine("[{0} {1}]", entry.Key.NameLocal, entry.Value); 
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Styles.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ConsoleApplication1
+{
+    class Styles
+    {
+        private Word.Application app;
+        private Word.Document doc=null;
+        private HashSet<Word.Style> set;
+        
+       
+        /*Constructor of the Base Class Styles
+         * This constructor takes in the filename of word file as parameter
+         */
+        public Styles(String filename)
+        {
+            app = new Word.Application();
+            doc = app.Documents.Open(filename);
+            app.Visible=false;
+            this.set = new HashSet<Word.Style>();
+            getStyles();    
+        }
+
+        /*This method retuns a Hashset of all the styles that are used in the word document
+         * In Simple words it returns a set of all Styles used in document.
+         */
+
+        public HashSet<Word.Style> getStyles()
+        {
+            foreach (Word.Style s in doc.Styles)
+            {
+                this.set.Add(s);
+            }
+            return this.set;
+        }
+
+        /*This method prints all the styles that were in the hash set of styles
+         *Warning :- This method should be called after getStyles() Method as the set would be empty if
+         * called earlier.
+         */
+
+
+        public void printStyles()
+        {
+            foreach (Word.Style s in this.set)
+            {
+                Console.WriteLine(s.NameLocal);
+            }
+        }
+
+
+        /*This method returns a Dictionary of type <Style, Based on Style>
+         * This method has a set of two styles, indicating that second one is based on first one.
+         */
+        public Dictionary<Word.Style, Word.Style> getBaseStyles()
+        {
+            Dictionary<Word.Style, Word.Style> dictionary_base_styles = new Dictionary<Word.Style, Word.Style>();
+            foreach (Word.Style s in doc.Styles)
+            {
+                dictionary_base_styles.Add(s, s.get_BaseStyle());
+            }
+            return dictionary_base_styles;
+        }
+
+        /*This method will return a dictionary of type <Style, Fontsize> used in that style
+         * This dictionary can be used to refer the font size of a particular style
+         */
+        public Dictionary<Word.Style, Double> getFontSizeByStyles()
+        {
+            Dictionary<Word.Style, Double> dictionary_font_size_by_styles = new Dictionary<Word.Style, double>();
+            foreach (Word.Style s in this.set)
+            {
+                dictionary_font_size_by_styles.Add(s, s.Font.Size);
+            }
+
+            return dictionary_font_size_by_styles;
+        }
+
+        /*This method returns a dictionary of type <Style, bool> where bool represents
+         * true or false based on the fact that bold italic or underline is used.
+         */
+        public Dictionary<Word.Style, bool> fontStyleUsed()
+        {
+            Dictionary<Word.Style,bool> dictionary_font_style_used = new Dictionary<Word.Style, bool>();
+            foreach (Word.Style s in this.set)
+            {
+                bool ans=false;
+                if (s.Font.Bold != 0 || s.Font.Italic != 0 || s.Font.Underline != 0)
+                {
+                    ans = true;
+                }
+                dictionary_font_style_used.Add(s, ans);
+            }
+            return dictionary_font_style_used;
+        }
+
+
+        /*This method will return a dictionary based on type <Style,bool> where bool represents true or false based 
+         * on the fact whether style is widow or not 
+         */
+        public Dictionary<Word.Style, bool> widowStyles()
+        {
+            Dictionary<Word.Style,bool> dictionary_of_widow_styles = new Dictionary<Word.Style, bool>();
+            foreach (Word.Style s in this.set)
+            {
+                bool ans = true;
+                if (s.ParagraphFormat.WidowControl != 1)
+                {
+                    ans = false;
+                }
+                dictionary_of_widow_styles.Add(s, ans);
+            }
+            return dictionary_of_widow_styles;
+        }
+
+        /*This method will return the dictionary based on type<Style, spacing before>
+         * where spacing before is a floating point value.
+         */
+        public Dictionary<Word.Style, float> getSpacingBefore()
+        {
+            Dictionary<Word.Style, float> dictionary_styles_spacing_before = new Dictionary<Word.Style, float>();
+            foreach (Word.Style s in this.set)
+            {
+                dictionary_styles_spacing_before.Add(s, s.ParagraphFormat.SpaceBefore);
+            }
+            return dictionary_styles_spacing_before;
+        }
+
+        /*This method will return the dictionary based on type<Style, spacing After>
+         * where spacing After is a floating point value.
+         */
+        public Dictionary<Word.Style, float> getSpacingAfter()
+        {
+            Dictionary<Word.Style, float> dictionary_styles_spacing_after = new Dictionary<Word.Style, float>();
+            foreach (Word.Style s in this.set)
+            {
+                dictionary_styles_spacing_after.Add(s, s.ParagraphFormat.SpaceAfter);
+            }
+            return dictionary_styles_spacing_after;
+        }
+
+
+    }
+}
