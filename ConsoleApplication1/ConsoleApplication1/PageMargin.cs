e36ff72a12483a5dc464d89c8409f3d6b34af72b
Bunty <bunty.newcastle@gmail.com>
2014-06-26 12:57:02 +0100

    Update Page margin
    Update page margin and rdfield

---
+++ ConsoleApplication1/ConsoleApplication1/PageMargin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ConsoleApplication1
+{
+    class PageMargin:Styles
+    {
+        private double top;
+        private double bottom;
+        private double right;
+        private double left;
+
+        public PageMargin(Word.Document doc, Word.Application app,double top_margin,double bottom_margin,
+            double left_margin,double right_margin)
+            : base(doc, app)
+        {
+            this.top = top_margin;
+            this.bottom = bottom_margin;
+            this.left = left_margin;
+            this.right = right_margin;
+        }
+
+        public bool runTop()
+        {
+            if (top <= 1.5 || top >= 3.2)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool runBottom()
+        {
+            if (bottom <= 1.5 || bottom >= 3.2)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool runLeft()
+        {
+            if (left <= 1.5 || left >= 3.5)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool runRight()
+        {
+            if (right <= 1.5 || right >= 3.2)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/RDFieldTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+namespace ConsoleApplication1
+{
+    class RDFieldTest:Styles
+    {
+        public RDFieldTest(Word.Document doc, Word.Application app)
+            : base(doc, app)
+        {
+
+        }
+
+        public bool rdFieldsTest()
+        {
+            Word.Fields f = doc.Fields;
+            bool usedRD = false;
+            foreach (Word.Field field in f)
+            {
+                Word.WdFieldType theType = field.Type;
+
+                if (theType == Word.WdFieldType.wdFieldRefDoc)
+                {
+                    usedRD = true;
+                    break;
+                }
+            }
+            return usedRD;
+        }
+    }
+}
