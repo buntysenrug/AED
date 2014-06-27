62baf12f112a3b696cf58096cdf383dd32d7e93b
Bunty <bunty.newcastle@gmail.com>
2014-06-27 13:36:59 +0100

    Started CaptionRef Test
    Started Caption Ref Class

---
+++ ConsoleApplication1/ConsoleApplication1/CaptionRefTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+namespace ConsoleApplication1
+{
+    class CaptionRefTest:Styles
+    {
+        public CaptionRefTest(Word.Document doc, Word.Application app)
+            : base(doc, app)
+        {
+
+        }
+    }
+}
