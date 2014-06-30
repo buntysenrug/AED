44d9cd642593b517daa12fd029fd25372e173a8a
Bunty <bunty.newcastle@gmail.com>
2014-06-30 16:49:20 +0100

    Psy Break
    Psy Break class added

---
+++ ConsoleApplication1/ConsoleApplication1/PsychologyPageBreak.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Word = Microsoft.Office.Interop.Word;
+namespace ConsoleApplication1
+{
+    class PsychologyPageBreak:Styles
+    {
+        private int countNPBAny;
+        private int countNPBIntroRef;
+
+        public PsychologyPageBreak(Word.Document doc, Word.Application app,int countPB,int countNPBRef)
+            : base(doc, app)
+        {
+            this.countNPBAny = countPB;
+            this.countNPBIntroRef=countNPBRef;
+        }
+
+        public bool runCountNPB()
+        {
+            if (countNPBAny == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool runNPBRef()
+        {
+            if (countNPBIntroRef == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
