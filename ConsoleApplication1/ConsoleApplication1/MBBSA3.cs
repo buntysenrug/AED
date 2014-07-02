6d0e8373e282074a68aa9c992c77c04cfcc58f70
Bunty <bunty.newcastle@gmail.com>
2014-07-02 16:49:00 +0100

    added dictionary of MBBs a3

--- ConsoleApplication1/ConsoleApplication1/MBBSA3.cs
+++ ConsoleApplication1/ConsoleApplication1/MBBSA3.cs
@@ -137,6 +137,64 @@ namespace ConsoleApplication1
             dictionary.Add("headingOneStyleTest_runNumbered", head1test.runNumbered());
             dictionary.Add("headingOneStyleTest_runBulleted", head1test.runBulleted());
             dictionary.Add("headingOneStyleTest_runTotalSpace", head1test.runTotalSpace());
+            //adding title test
+            dictionary.Add("titleStyleTests_runTitleNotTwice" ,titletest.runTitleNotTwice(paratest.getStylesInDoc(), paratest.getTitleCount()));
+            //heading 2 test
+            dictionary.Add("headingTwoStyleTest_runInUse", head2test.runInUse());
+            dictionary.Add("headingTwoStyleTest_runBase", head2test.runBase());
+            dictionary.Add("headingTwoStyleTest_runOutline", head2test.runOutline());
+            dictionary.Add("headingTwoStyleTest_runKeep", head2test.runKeep());
+            dictionary.Add("headingTwoStyleTest_runNumbered", head2test.runNumbered());
+            dictionary.Add("headingTwoStyleTest_runBulleted", head2test.runBulleted());
+            dictionary.Add("headingTwoStyleTest_runTotalSpace", head2test.runTotalSpace());
+            //heading 3 tests
+            dictionary.Add("headingThreeStyleTest_runInUse", head3test.runInUse());
+            dictionary.Add("headingThreeStyleTest_runBase", head3test.runBase());
+            dictionary.Add("headingThreeStyleTest_runOutline", head3test.runOutline());
+            dictionary.Add("headingThreeStyleTest_runKeep", head3test.runKeep());
+            dictionary.Add("headingThreeStyleTest_runNumbered", head3test.runNumbered());
+            dictionary.Add("headingThreeStyleTest_runBulleted", head3test.runBulleted());
+            dictionary.Add("headingThreeStyleTest_runTotalSpace", head3test.runTotalSpace());
+            //heading order test
+            dictionary.Add("headingOrderTest", paratest.headingOrderTest());
+            //normal style test
+            dictionary.Add("normalStyleTest_runInUse", normal.runInUse());
+            dictionary.Add("normalStyleTest_runBase", normal.runBase());
+            dictionary.Add("normalStyleTest_runOutline", normal.runOutline());
+            dictionary.Add("normalStyleTest_runKeep", normal.runKeep());
+            dictionary.Add("normalStyleTest_runFontEffects", normal.runFontEffects());
+            dictionary.Add("normalStyleTest_runFontSize", normal.runFontSize());
+            dictionary.Add("normalStyleTest_runFontStyle", normal.runFontStyle());
+            dictionary.Add("normalStyleTest_runTotalSpace", head3test.runTotalSpace());
+            //paragraph test
+            dictionary.Add("paragraphStyleTest", paratest.paragraphStyleTest(3));
+            //subtitle test
+            dictionary.Add("subtitleStyleUsedTest", subtitle.subTitileStyleUsedTest(paratest.getSubtitleQuotes()));
+            //character style quotes
+            dictionary.Add("characterStyleTest",character.characterStyleTest(paratest.getCharacterQuotes()));
+            //normal web style
+            dictionary.Add("normalWebStyleUsedTest",normalweb.normalWebStyleUsedTest());
+            //spacing tests
+            dictionary.Add("spacingTests_runCarriage",spacetest.runCarriage());
+            dictionary.Add("spacingTests_runCarriageSingle",spacetest.runCarriageSingle());
+            dictionary.Add("spacingTests_runBreakingMiddle",spacetest.runBreakingMiddle());
+            dictionary.Add("spacingTests_runBreakingStart", spacetest.runBreakingStart());
+            dictionary.Add("spacingTests_runTabStart", spacetest.runTabsStart());
+            dictionary.Add("spacingTests_runTabConsec", spacetest.runTabsConsec());
+            dictionary.Add("spacingTests_runShiftEnters",spacetest.runShiftEnters());
+            //quote tests
+            dictionary.Add("quoteStyleTest_runBase", quotetest.runBase());
+            dictionary.Add("quoteStyleTest_runFontStyle", quotetest.runFontStyle());
+            dictionary.Add("quoteStyleTest_runSpaceA", quotetest.runSpaceA());
+            dictionary.Add("quoteStyleTest_runIndent", quotetest.runIndent());
+            //header style test
+            dictionary.Add("headerStyleUsedTest", headerstyle.headerStyleUsedTest());
+            //footer style test
+            dictionary.Add("footerStyleUsedTest", footerstyle.footerStyleUsedTest());
+            //student number test
+            dictionary.Add("studentNumberTest", studentNo.studentNumberTest());
+            //styles in use test
+            dictionary.Add("stylesInUseTest", paratest.stylesInUseTest());
 
             return dictionary;
         }
